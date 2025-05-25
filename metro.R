204ceb3ea820fff9ffab8c00ca5f1aa249d0c557
Tawfik14 <131150689+Tawfik14@users.noreply.github.com>
2025-05-25 22:50:13 +0200

    Update metro.R

--- metro.R
+++ metro.R
@@ -10,26 +10,18 @@ data <- read_delim("qualite-de-lair-dans-le-reseau-de-transport-francilien.csv",
 # Afficher les noms de colonnes pour vérification
 names(data)
 
-#Nettoyage dataset
-# Remplacer les chaînes vides par NA
+#Nettoyage dataset en remplaçant les chaînes vides par NA
 data[data == ""] <- NA
 
-# Supprimer les lignes où toutes les valeurs sont NA (cette commande ne fait rien car pas de lignes inutiles)
+# On va supprimer les lignes où toutes les valeurs sont NA 
 data <- data[rowSums(is.na(data)) != ncol(data), ]
 
-# Supprimer les colonnes où toutes les valeurs sont NA
+# On va supprimer les colonnes où toutes les valeurs sont NA
 data <- data[, colSums(is.na(data)) != nrow(data)]
 
-# Supprimer les lignes en double ( de même elle ne fait rien on le remarque car le dataset n'espas modifier)
+# On va supprimer les lignes en double 
 data <- unique(data)
 
-#acp
-#summary(data)
-#str(data)
-
-#acp=prcomp(data, scale. = TRUE)
-#summary(acp)
-#biplot(acp)
 
 #filtrer dataset
 metro_data <- data[grepl("Métro", data$`Nom de la ligne`),ignore.case = TRUE, ]
